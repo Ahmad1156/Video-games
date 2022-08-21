e2a8ebb9d6c13eb72287af0751f3e2cbca3db9a4
ahmad shakik <ahmadshakik777@gmail.com>
2022-08-21 12:34:08 +0300

    Deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
+  Headers_key: 'ed87dd5d2dmsh89593d9ce768fd8p10b7ebjsn211e3d25ad75',
+  API_KEY: '0a1003f55a5745a28cf5c1737c099058'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
